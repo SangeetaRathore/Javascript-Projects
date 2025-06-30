2bb2be5f4975f6c6a0c0d04171258672a575a576
SangeetaRathore <sangeetarathore23march@gmail.com>
2025-06-30 12:30:36 +0530

    completed

--- To-Do/script.js
+++ To-Do/script.js
@@ -3,19 +3,52 @@ const toDoBox = document.querySelector('#to-do-list')
 
 
 item.addEventListener('keyup',function(e){
-    
     if(e.key=='Enter'){
         addToDo(this.value)
         this.value = ""
-    }
-    
+    }   
 })
 
 
-const addToDo = (item) =>{
+
+const addToDo = (text) =>{
+
+    // Create the task functionality 
     const listItem = document.createElement('li');
-    listItem.innerHTML = `${item}
-                    <i class="fa-solid fa-xmark"></i>`
-   toDoBox.appendChild(listItem)
+    listItem.innerHTML = `<div class="text">${text}</div>
+                    <i class="fa-solid fa-xmark"></i>
+                    <button id="button">Edit</button>`
+
+    // add the task in list box
+    toDoBox.appendChild(listItem)
+
+
+
+
+    // To show the task is done  
+    listItem.addEventListener('click',function(){
+        this.classList.toggle('done')
+    })      
+    
+
+
+
+    // Remove the task functionality
+    listItem.querySelector('i').addEventListener('click',function(){
+        listItem.remove() 
+    })
+
+    
+
+    // Edit the task functionality
+     listItem.querySelector('#button').addEventListener('click', function() {
+        const currentText = listItem.querySelector('.text').innerText;
+        item.value = currentText;    // set input box to current text
+        listItem.remove();           // remove old item
+        item.focus();                // focus input for quick editing
+        
+    });
+
+
 }
 
